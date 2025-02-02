733a931001694b04412cd38c35da02015f042dca
Omkar Damame <omkardamame.work@gmail.com>
2025-02-02 19:32:42 +0530

    Create userdata_docker.sh

---
+++ userdata_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+apt update
+apt install htop -y
+apt install vim -y
+apt install dnsutils -y
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+USER=$(whoami)
+usermod -aG docker $USER
+systemctl enable docker --now
