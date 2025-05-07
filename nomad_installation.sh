0711ca83aab6576c6c2162e8368a3463ffc4f0ca
Omkar Damame <omkardamame.work@gmail.com>
2025-05-07 23:55:22 +0530

    Create nomad_installation.sh

---
+++ nomad_installation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update
+apt install gpg htop -y
+wget -O - https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install nomad -y
+systemctl start nomad
