5a398ffb3eecc45ce05b31e544abd4eba7e8aa6f
omkardamame <omkardamame.work@gmail.com>
2025-06-17 19:15:08 +0530

    Created bash script of docker for jenkins

---
+++ docker_installation_for_jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt update
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+apt update
+apt install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+usermod -aG docker admin
+usermod -aG docker jenkins
+systemctl enable docker --now
